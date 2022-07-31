fac44202b8fb58ef6ae1bb839c562bfcb0831e77
johnkdunyo <johnkdunyo@gmail.com>
2022-07-31 13:54:32 +0000

    added connection to remote db

--- src/data-source.ts
+++ src/data-source.ts
@@ -4,10 +4,10 @@ import { User } from "./entity/User"
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
+    host: "eskilldb.ctwk3dnfgvwn.us-west-2.rds.amazonaws.com",
     port: 5432,
     username: "postgres",
-    password: "root",
+    password: "E62EV5wX^2h7!28ET$2!c^%79",
     database: "eskilldb",
     synchronize: true,
     logging: false,
@@ -15,3 +15,19 @@ export const AppDataSource = new DataSource({
     migrations: [],
     subscribers: [],
 })
+
+
+// export const AppDataSource = new DataSource({
+//     type: "postgres",
+//     host: "localhost",
+//     port: 5432,
+//     username: "postgres",
+//     password: "root",
+//     database: "eskilldb",
+//     synchronize: true,
+//     logging: false,
+//     entities: ["src/entity/**/*.ts"],
+//     migrations: [],
+//     subscribers: [],
+// })
+
