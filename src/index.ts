58311d4747ff97ca4a7a163e86771523bb659bf1
micky-quansah <52993196+micky-quansah@users.noreply.github.com>
2022-07-31 22:49:26 +0000

    edited index.ts file to add process port

--- src/index.ts
+++ src/index.ts
@@ -100,7 +100,7 @@ AppDataSource.initialize()
         }
     );
 
-    const port = 3000
+    const port = process.env.PORT || 3000
 
     app.listen(port);
     console.log(`Express server has started on port ${port}.`);
